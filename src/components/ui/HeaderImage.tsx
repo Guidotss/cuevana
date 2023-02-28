a0c1c5e0fbb5c2e0dada41533c9012a786d0e848
Guidotss <guidoolguin@hotmail.com>
2023-02-28 20:52:48 -0300

    Arreglando imagen de fondo en HeaderImage

--- src/components/ui/HeaderImage.tsx
+++ src/components/ui/HeaderImage.tsx
@@ -11,32 +11,41 @@ interface Props {
 export const HeaderImage: FC<Props> = ({ film }) => {
   return (
     <Box>
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        sx={{
-          mt:'-100px',
-          width: "100%",
-          height: "600px",
-          backgroundImage: `url(https://image.tmdb.org/t/p/original${film.backdrop_path})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          position: "relative",
-          zIndex: -1,
-          '&::after':{
-            content: '""',
-            position: "absolute",
-            top: '60vh',
-            left: 0,
+       <Box
+          sx={{
+            position: "relative",
             width: "100%",
-            height: "100px",
-            background: "linear-gradient(180deg,rgba(20,26,50,0) 0%,#141a32 100%)",
-          }
-        }}
-      >
+            height: "600px",
+            zIndex: -1,
+            '&::after': {
+              background:'linear-gradient(180deg,rgba(8,15,40,0) 0,#080f28)',
+              content:"",
+              position:'absolute',
+              left:0,
+              bottom:0,
+              right:0,
+              height:'100%',
+              pointer-events:'none',
+
+            }
+          }}       
+        >
+          <Image
+            src={`https://image.tmdb.org/t/p/original${film.backdrop_path}`}
+            alt={film.title}
+            width={1909}
+            height={600}
+            quality={100}
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+              zIndex: -1,
+              position: "relative",
+              marginTop:'-6.5rem'
+            }}
+          />
       </Box>
     </Box>
+            
   );
 };
