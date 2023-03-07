49fa0764930aa945343097ff569cc129e432dbf9
Guidotss <guidoolguin@hotmail.com>
2023-03-07 13:07:56 -0300

    Actualizando next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['m.media-amazon.com','image.tmdb.org']
+  },
+  env:{
+    API_KEY_TMDB: process.env.API_KEY_TMDB,
+    NEXT_PUBLIC_API_KEY_TMDB: process.env.NEXT_PUBLIC_API_KEY_TMDB,
   }
 }
 
